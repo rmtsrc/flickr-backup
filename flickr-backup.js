bae519280578587bf98757d45d3cc95c6f82c8ec
Seb Flippence <144435+SebFlippence@users.noreply.github.com>
2013-01-13 22:09:14 +0000

    Added initial tests for config loading and listing assets from the file system

---
+++ flickr-backup.js
@@ -0,0 +1,122 @@
+(function () {
+    var _config = {
+        'supportedFiletypes':'^.*\.(jpg|JPG|jpeg|JPEG|png|PNG)$'
+    };
+
+    /**
+     * Loads the configuration file from disc
+     *
+     * @param file
+     * @return {Object}
+     */
+    function loadConfig(file) {
+        file = file || './config';
+
+        var config = require(file);
+
+        return {
+            get:function (key) {
+                return config[key];
+            }
+        }
+    }
+
+    /**
+     * Returns an object of files from a given path recursively, that match a given regex
+     *
+     * @param path - system path
+     * @param type - limit to given regex
+     * @return {Object}
+     * @private
+     */
+    function _listFilesFromDir(path, type) {
+        var files = {},
+            i = 0;
+
+        var DirReader = require('dirreader');
+
+        DirReader.readDirSync(path, function (err, fullPath, stats, ifDir) {
+            if (err) {
+                throw err;
+            }
+
+            // Only add if its a file & matches given filetype
+            if (!ifDir && fullPath.match(type)) {
+                // Convert to a relative path
+                var relativePath = (function () {
+                    var posOfRelativePath = fullPath.search(path);
+                    var relativePath = fullPath.substring(posOfRelativePath + 1, fullPath.length);
+                    // Remove any slashes at the start
+                    if (relativePath.charAt(0) === '/') {
+                        relativePath = relativePath.substring(1);
+                    }
+                    return relativePath;
+                })();
+
+                files[i] = {
+                    'path' : relativePath
+                };
+                i++;
+            }
+        });
+
+        return files;
+    }
+
+    /**
+     * Takes an input file path and converts to path to tags (removing the initial directory)
+     *
+     * @param dirPath
+     * @param filePath
+     * @return {Array}
+     * @private
+     */
+    function _convertPathToTags(dirPath, filePath) {
+        dirPath = dirPath.replace(/^\./, '');
+        dirPath = dirPath.replace(/^\//, '');
+        filePath = filePath.replace(dirPath, '');
+
+        var fileParts = filePath.split('/'),
+            tags = [],
+            i = 1;
+        for (part in fileParts) {
+            var filePart = fileParts[part];
+            if (filePart !== '' && fileParts.length !== i) {
+                tags.push(filePart);
+            }
+
+            i++;
+        }
+
+        return tags;
+    }
+
+    /**
+     * Returns list of files and path as tags
+     *
+     * @param path
+     * @return {Array}
+     */
+    function getAssetList(path) {
+        path = path || '.';
+
+        var fileList = _listFilesFromDir(path, _config.supportedFiletypes),
+            i = 0;
+        for (file in fileList) {
+            fileList[i]['tags'] = _convertPathToTags(path, fileList[file].path);
+            i++;
+        }
+
+        return fileList;
+    }
+
+    /**
+     * Public methods
+     *
+     * @type {Object}
+     */
+    module.exports = {
+        'loadConfig':loadConfig,
+        'getAssetList':getAssetList
+    };
+})();
---
+++ test/fixtures/config.js
@@ -0,0 +1,12 @@
+(function () {
+    var config = {
+        "default_upload_privacy":{
+            "is_public":0,
+            "is_friend":0,
+            "is_family":0
+        },
+        "add_folders_as_tags":true
+    };
+
+    module.exports = config;
+})();
